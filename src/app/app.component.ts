c8b55d78813eb2907dfa58a066b10fffb35e2383
Oussama Harrathi <oussamaharrathi333@gmail.com>
2024-01-03 22:55:59 +0100

    save5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,38 +8,51 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit, OnDestroy {
   isLoading: boolean = false;
-  private readonly appInstanceId = 'appInstance';
+  private readonly loginEvent = 'loginEvent';
+
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en'); // Set default language to English
     translate.use('en'); // Use English translations
   }
+
   ngOnInit() {
-    if (!localStorage.getItem(this.appInstanceId)) {
-      localStorage.setItem(this.appInstanceId, 'true');
-      window.addEventListener('beforeunload', this.clearLocalStorage);
-      window.addEventListener('storage', this.onStorageEvent);
-    }
+    // Listen for login events in other tabs
+    window.addEventListener('storage', this.onStorageEvent);
   }
 
   ngOnDestroy() {
-    this.clearLocalStorage();
+    window.removeEventListener('storage', this.onStorageEvent);
   }
 
-  private clearLocalStorage = () => {
-    localStorage.removeItem(this.appInstanceId);
-    window.removeEventListener('beforeunload', this.clearLocalStorage);
-    window.removeEventListener('storage', this.onStorageEvent);
-  };
+  // Method to handle login (this should be called when login is successful)
+  handleLogin() {
+    // Set item in localStorage to broadcast login event
+    localStorage.setItem(this.loginEvent, new Date().toString());
+  }
+
+  // Method to handle logout (this should be called when logout is successful)
+  handleLogout() {
+    // Clear item in localStorage to broadcast logout event
+    localStorage.removeItem(this.loginEvent);
+  }
+
   showLoader() {
     this.isLoading = true;
   }
+
   hideLoader() {
     this.isLoading = false;
   }
+
   private onStorageEvent = (event: StorageEvent) => {
-    if (event.key === this.appInstanceId && !event.newValue) {
-      window.removeEventListener('storage', this.onStorageEvent);
-      window.location.reload(); // Refresh the tab if the other instance is closed
+    if (event.key === this.loginEvent) {
+      // React to login event, e.g., fetch user data, update UI
+      this.updateOnLogin();
     }
   };
+
+  private updateOnLogin() {
+    // Implement logic to update the app state on login
+    window.location.reload();
+  }
 }
